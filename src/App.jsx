4723fd359a7ca5714da39d53ddf092b07a75ce77
zarawiicu <zarasaa05@gmail.com>
2024-10-16 06:41:28 +0700

    Praktikum 3.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,50 @@
 //import React from 'react';
 import { useState } from "react";
 import "./index.css";
-import Form from "./components/Form"; //file Form.jsx ada di folder components yang dibuat sebelumnya
-import InputField from "./components/InputField"; // file InputFiled.jsx ada di folder components
-
-function App() {
-  const [name, setName] = useState("");
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    alert(`Submitted: ${name}`);
+export default function App() {
+  const [state, setState] = useState("");
+  const [stateNim, setStateNim] = useState("");
+  const handleSubmit = () => {
+    alert(`Hi ${state}, with NIM ${stateNim}`);
+  };
+  const handleChange = (e) => {
+    setState(e.target.value);
+  };
+  const handleNimChange = (e) => {
+    setStateNim(e.target.value);
   };
   return (
     <div className="h-screen flex items-center justify-center bg-gray-100">
-      <Form onSubmit={handleSubmit}>
-        <InputField
-          label="Name"
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <button
+      <form onSubmit={handleSubmit} className="bg-white p-6 rounded shadow-lg">
+        <h1 className="text-center">Masukan Nama</h1>
+        <div className="mb-4">
+          <label className="block text-gray700">
+            Nama:
+            <input
+              type="text"
+              value={state}
+              onChange={handleChange}
+              className="border border-gray-300 p-2 w-full"
+            />
+          </label>
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray700">
+            NIM :
+            <input
+              type="text"
+              value={stateNim}
+              onChange={handleNimChange}
+              className="border border-gray-300 p-2 w-full"
+            />
+          </label>
+        </div>
+        <input
           type="submit"
-          className="px-4 py-2 bg-blue-500 
-text-white rounded"
-        >
-          Submit
-        </button>
-      </Form>
+          className="px-4 py-2 bg-blue-500 text-white rounded"
+          value="Submit"
+        />
+      </form>
     </div>
   );
 }
-export default App;
