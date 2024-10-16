52d1c5ecf28d20c6012bfee298c1b999b755b997
zarawiicu <zarasaa05@gmail.com>
2024-10-16 07:14:50 +0700

    Praktikum 3.3

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,54 @@
-//import React from 'react';
-import { useState } from "react";
+import React, { useState } from "react";
 import "./index.css";
+
 export default function App() {
-  const [state, setState] = useState("");
-  const [stateNim, setStateNim] = useState("");
-  const handleSubmit = () => {
-    alert(`Hi ${state}, with NIM ${stateNim}`);
-  };
-  const handleChange = (e) => {
-    setState(e.target.value);
-  };
-  const handleNimChange = (e) => {
-    setStateNim(e.target.value);
+  const fileInput = React.createRef();
+  const [imageSrc, setImageSrc] = useState(null);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const file = fileInput.current.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImageSrc(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+    alert(`Nama file: ${file.name}`);
   };
   return (
     <div className="h-screen flex items-center justify-center bg-gray-100">
       <form onSubmit={handleSubmit} className="bg-white p-6 rounded shadow-lg">
-        <h1 className="text-center">Masukan Nama</h1>
+        <h1 className="text-center text-bold">Upload File</h1>
         <div className="mb-4">
-          <label className="block text-gray700">
-            Nama:
+          <label className="block text-gray-700">
+            Pilih File
             <input
-              type="text"
-              value={state}
-              onChange={handleChange}
+              type="file"
+              ref={fileInput}
+              style={{ marginLeft: "5px" }}
               className="border border-gray-300 p-2 w-full"
             />
           </label>
         </div>
-        <div className="mb-4">
-          <label className="block text-gray700">
-            NIM :
-            <input
-              type="text"
-              value={stateNim}
-              onChange={handleNimChange}
-              className="border border-gray-300 p-2 w-full"
-            />
-          </label>
-        </div>
-        <input
+        <button
           type="submit"
-          className="px-4 py-2 bg-blue-500 text-white rounded"
-          value="Submit"
-        />
+          style={{ marginLeft: "5px" }}
+          className="bg-blue-500 text-white py-2 px-4 rounded"
+        >
+          Submit
+        </button>
       </form>
+      {imageSrc && (
+        <div style={{ marginTop: "20px" }}>
+          <h2>Preview Gambar:</h2>
+          <img
+            src={imageSrc}
+            alt="Preview"
+            style={{ maxWidth: "100%", height: "auto" }}
+          />
+        </div>
+      )}
     </div>
   );
 }
